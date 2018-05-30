59258caeaa97b3fea88bdf6a9735f6bbadbd4e87
Matthew Taylor <mattjktaylor@outlook.com>
2018-05-30 12:41:32 +0100

    Change area colour to dark grey

--- nodeJsServer/public/js/mapFunctions.js
+++ nodeJsServer/public/js/mapFunctions.js
@@ -550,8 +550,8 @@ function addTransparentPolygon(ID, lineColour, coords){
 		id: ID,
 		paths: coords,
 		strokeColor: lineColour,
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
+        strokeOpacity: 0.6,
+        strokeWeight: 1,
         fillColor: '#000000',
         fillOpacity: 0.0,
 		clickable: false
